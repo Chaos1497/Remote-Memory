169495f28a8591c3508e2d6ebf6593eefb11dd17
Esteban Zúñiga <zunigaesteban@hotmail.com>
2017-09-24 16:56:07 -0600

    Add files via upload
    Ejemplo de prueba para RMlib

---
+++ Ejemplo/main.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <stdlib.h>
+#include "/home/esteban/RMlib/rmlib.cpp"
+
+using namespace std;
+
+string llave;
+string valor;
+
+void menu(){
+    cout<<"\n\t\tAdministrador de datos\n\n";
+    cout<<"Aquí puede guardar, eliminar y buscar sus datos"<<endl;
+    cout<<" 1. Guardar Dato"<<endl;
+    cout<<" 2. Buscar Dato"<<endl;
+    cout<<" 3. Eliminar Dato"<<endl;
+
+    cout<<"\n Ingrese una opción: ";
+}
+
+void guardar(){
+    cout<<"Ingrese el dato a guardar"<<endl;
+    cout<<"Maximo 6 caracteres"<<endl;
+    cout<<"Si faltan campos llénelos con espacios"<<endl;
+    cin>>llave;
+    cout<<"Ingrese el cilindraje"<<endl;
+    cin>>valor;
+    while(true){
+        sendData(llave,valor);
+        break;
+    }
+    menu();
+}
+
+void buscar(){
+    cout<<"Ingrese el dato a buscar"<<endl;
+    cout<<"Maximo 6 caracteres"<<endl;
+    cout<<"Si faltan campos llénelos con espacios"<<endl;
+    cin>>llave;
+    cout<<"Ingrese el cilindraje"<<endl;
+    cin>>valor;
+    while(true){
+        searchData(llave,valor);
+        break;
+    }
+    menu();
+}
+
+void eliminar(){
+    cout<<"Ingrese el dato a eliminar"<<endl;
+    cout<<"Maximo 6 caracteres"<<endl;
+    cout<<"Si faltan campos llénelos con espacios"<<endl;
+    cin>>llave;
+    cout<<"Ingrese el cilindraje"<<endl;
+    cin>>valor;
+    while(true){
+        deleteData(llave,valor);
+        break;
+    }
+    menu();
+}
+
+int main() {
+    rm_init(1500, 1501, "192.168.1.2", "192.168.1.2");
+    int opcion;
+
+    do{
+        menu();
+        cin>> opcion;
+
+        switch(opcion){
+            case 1:
+                guardar();
+                break;
+
+            case 2:
+                buscar();
+                break;
+
+            case 3:
+                eliminar();
+                break;
+        }
+    }while(opcion!=4);
+
+    system("pause");
+    return 0;
+}
